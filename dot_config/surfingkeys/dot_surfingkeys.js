8ddbe3bf37a74c4ca4b6c389e61aaa8164597cb5
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2021-07-31 21:08:59 +0100

    Update dot_config/surfingkeys/dot_surfingkeys.js

--- dot_config/surfingkeys/dot_surfingkeys.js
+++ dot_config/surfingkeys/dot_surfingkeys.js
@@ -5,3 +5,12 @@ map("L", "D");
 // settings.smoothScroll = false;
 // settings.scrollStepSize = 140;
 
+http://192.168.0.1/index.html#login
+mapkey('<Ctrl-l>', 'fill in login form', function() {
+ const origin = window.location.origin
+ if(origin === "some-specific-domain"){           
+  window.document.getElementById("username").value = username
+   window.document.getElementById("password").value = password 
+   window.document.getElementById("loginButton").click() 
+   return 
+ }
