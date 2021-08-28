a72f20852758c19ea66caa441884fa16b4801a30
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2021-08-28 21:06:21 +0100

    Add dot_config/nvim/lua/config/colorizer.lua Add dot_config/nvim/lua/config/gitsigns.lua Add dot_config/nvim/lua/config/hop.lua Add dot_config/nvim/lua/config/lightspeed.lua Add dot_config/nvim/lua/config/neoscroll.lua Add dot_config/nvim/lua/config/nvim-autopairs.lua Add dot_config/nvim/lua/config/nvim-comment.lua Add dot_config/nvim/lua/config/telescope.lua Add dot_config/nvim/lua/galaxyline/eviline.lua Add dot_config/nvim/lua/galaxyline/my_providers/flags.lua Add dot_config/nvim/lua/galaxyline/my_theme.lua Add dot_config/nvim/lua/galaxyline/spaceline.lua

---
+++ dot_config/nvim/lua/config/nvim-autopairs.lua
@@ -0,0 +1,55 @@
+require('nvim-autopairs').setup({
+    map_bs = true;  -- map the <BS> key
+    disable_filetype = { "TelescopePrompt" };
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]],"%s+", "");
+    enable_moveright = true;
+    enable_afterquote = true;  -- add bracket pairs after quote
+    enable_check_bracket_line = true;  --- check bracket in same line
+    check_ts = true;
+	fast_wrap = {
+      map = '<c-f>',
+      chars = { '{', '[', '(', '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+      end_key = '$',
+      keys = 'qwertyuiopzxcvbnmasdfghjkl',
+      check_comma = true,
+      hightlight = 'Search'
+    },
+})
+
+-- compe
+require("nvim-autopairs.completion.compe").setup({
+  map_cr = true, --  map <CR> on insert mode
+  map_complete = true, -- it will auto insert `(` after select function or method item
+  auto_select = false,  -- auto select first item
+})
+
+local npairs = require'nvim-autopairs'
+local Rule   = require'nvim-autopairs.rule'
+
+-- add space between paranthesis
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']')
+}
