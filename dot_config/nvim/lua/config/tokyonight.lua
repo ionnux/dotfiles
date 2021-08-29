e148906f69151771cdbb044308f76be99a0ff6fc
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2021-08-29 05:07:07 +0100

    Add dot_config/nvim/lua/config/compe.lua Add dot_config/nvim/lua/config/tokyonight.lua

---
+++ dot_config/nvim/lua/config/tokyonight.lua
@@ -0,0 +1,17 @@
+vim.g.tokyonight_style = "night"
+--vim.g.tokyonight_day_brightness = 0.1
+vim.g.tokyonight_terminal_colors = false
+vim.g.tokyonight_italic_comments = true
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_keywords = true
+vim.g.tokyonight_italic_variables = true
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_lualine_bold = 1
+vim.g.tokyonight_hide_inactive_statusline = true
+--vim.g.tokyonight_sidebars = {"NvimTree"}
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_dark_sidebar = false
+vim.g.tokyonight_dark_float = true
+vim.g.tokyonight_lualine_bold = true
+
+vim.cmd[[colorscheme tokyonight]]
