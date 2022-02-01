e932345247fb59f453a2d7b37de726c923ba2608
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-02-01 16:00:56 +0100

    Update dot_config/i3/config Update dot_config/i3/i3Scripts/executable_kill.sh Add dot_config/i3/i3Scripts/executable_terminal_scratchpad.sh Update dot_config/kitty/kitty.conf Update dot_config/picom/picom.conf Update dot_config/private_mpv/mpv.conf

--- dot_config/i3/i3Scripts/executable_kill.sh
+++ dot_config/i3/i3Scripts/executable_kill.sh
@@ -2,18 +2,30 @@
 
 windowname=$(xdotool getactivewindow getwindowname)
 
-if [[ "$windowname" == "dropdown_terminal2" ]]
-then
-    if pgrep -a kitty | grep dropdown_terminal1
-    then
-        i3-msg '[title="dropdown_terminal1"] move absolute position 15 px 47 px, resize set 1890 px 850 px'
-        i3-msg '[title=dropdown_terminal2] kill'
-    else
-        i3-msg '[title=dropdown_terminal2] kill'
-    fi
-else
-    i3-msg 'kill'
-fi
+# if [[ "$windowname" == "dropdown_terminal2" ]]
+# then
+#     if pgrep -a kitty | grep dropdown_terminal1
+#     then
+#         i3-msg '[title="dropdown_terminal1"] move absolute position 15 px 47 px, resize set 1890 px 850 px'
+#         i3-msg '[title=dropdown_terminal2] kill'
+#     else
+#         i3-msg '[title=dropdown_terminal2] kill'
+#     fi
+# else
+#     i3-msg 'kill'
+# fi
 # if pgrep -a kitty | grep dropdown_terminal1
 # then
 
+case "$windowname" in
+    "dropdown_terminal1")
+        i3-msg '[title="dropdown_terminal2"] move absolute position 15 px 47 px, resize set 1890 px 850 px'
+        i3-msg '[title=dropdown_terminal1] kill'
+        ;;
+    "dropdown_terminal2")
+        i3-msg '[title="dropdown_terminal1"] move absolute position 15 px 47 px, resize set 1890 px 850 px'
+        i3-msg '[title=dropdown_terminal2] kill'
+        ;;
+    *)
+        i3-msg 'kill'
+esac
