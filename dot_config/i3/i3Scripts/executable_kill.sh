2477f1dbe9b6a003540301ec8235c3a02242abf4
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-02-02 13:33:46 +0100

    changes made

--- dot_config/i3/i3Scripts/executable_kill.sh
+++ dot_config/i3/i3Scripts/executable_kill.sh
@@ -2,29 +2,14 @@
 
 windowname=$(xdotool getactivewindow getwindowname)
 
-# if [[ "$windowname" == "dropdown_terminal2" ]]
-# then
-#     if pgrep -a kitty | grep dropdown_terminal1
-#     then
-#         i3-msg '[title="dropdown_terminal1"] move absolute position 15 px 47 px, resize set 1890 px 850 px'
-#         i3-msg '[title=dropdown_terminal2] kill'
-#     else
-#         i3-msg '[title=dropdown_terminal2] kill'
-#     fi
-# else
-#     i3-msg 'kill'
-# fi
-# if pgrep -a kitty | grep dropdown_terminal1
-# then
-
 case "$windowname" in
-    "terminal [1]")
-        i3-msg '[title="terminal \[2\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
-        i3-msg '[title="terminal \[1\]"] kill'
+    "dropdown_terminal [1]")
+        i3-msg '[title="dropdown_terminal \[2\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
+        i3-msg '[title="dropdown_terminal \[1\]"] kill'
         ;;
-    "terminal [2]")
-        i3-msg '[title="terminal \[1\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
-        i3-msg '[title="terminal \[2\]"] kill'
+    "dropdown_terminal [2]")
+        i3-msg '[title="dropdown_terminal \[1\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
+        i3-msg '[title="dropdown_terminal \[2\]"] kill'
         ;;
     *)
         i3-msg 'kill'
--- dot_config/i3/i3Scripts/executable_scratchpad.sh
+++ dot_config/i3/i3Scripts/executable_scratchpad.sh
@@ -2,46 +2,140 @@
 
 previous_window=$(xdotool getactivewindow getwindowname)
 
+split_screen_between_terminals () {
+    case "$1" in
+        "top")
+            # split the screen between dropdown_terminal [1] and dropdown_terminal [2]
+            i3-msg '[title="dropdown_terminal \[2\]"] move absolute position 967 px 47 px, resize set 939 px 900 px'
+            i3-msg '[title="dropdown_terminal \[1\]"] move absolute position 14 px 47 px, resize set 939 px 900 px'
+            ;;
+
+        "right")
+            # split the screen between dropdown_terminal [1] and dropdown_terminal [2]
+            i3-msg '[title="dropdown_terminal \[1\]"] move absolute position 967 px 47 px, resize set 939 px 502 px'
+            i3-msg '[title="dropdown_terminal \[2\]"] move absolute position 967 px 563 px, resize set 939 px 502 px'
+            ;;
+    esac
+}
+
+make_terminal_full_screen () {
+    terminal=""
+
+    case "$1" in
+        "dropdown_terminal [1]")
+            terminal="1"
+            ;;
+        "dropdown_terminal [2]")
+            terminal="2"
+            ;;
+    esac
+
+    case "$2" in
+        "top")
+            i3-msg '[title="dropdown_terminal \['$terminal'\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px' # make dropdown_terminal [1] fullscreen
+            ;;
+
+        "right")
+            i3-msg '[title="dropdown_terminal \['$terminal'\]"] move absolute position 967 px 47 px, resize set 939 px 1018 px' # make dropdown_terminal [2] fullscreen
+            ;;
+    esac
+}
+
 case "$1" in
-    "terminal [1]")
-        i3-msg '[title="terminal \[1\]"] scratchpad show'
+    "dropdown_terminal [1]")
+
+        i3-msg '[title="dropdown_terminal \[1\]"] scratchpad show'
         i3-msg '[title="cmus"] move scratchpad'
 
         sleep 0.1
         current_window=$(xdotool getactivewindow getwindowname)
 
-        if [ "$current_window" = "terminal [1]" ]
+        if [ "$current_window" = "dropdown_terminal [1]" ]  # dropdown_terminal [1] is being shown
         then
-            if [ "$previous_window" = "terminal [2]" ]
+            if [ "$previous_window" = "dropdown_terminal [2]" ] # dropdown_terminal [2] is already visible
             then
-                i3-msg '[title="terminal \[2\]"] move absolute position 967 px 47 px, resize set 939 px 900 px'
-                i3-msg '[title="terminal \[1\]"] move absolute position 14 px 47 px, resize set 939 px 900 px'
+                case "$2" in
+                    "top")
+                        # split the screen between dropdown_terminal [1] and dropdown_terminal [2]
+                        split_screen_between_terminals "top"
+                        ;;
+
+                    "right")
+                        # split the screen between dropdown_terminal [1] and dropdown_terminal [2]
+                        split_screen_between_terminals "right"
+                        ;;
+                esac
             else
-                i3-msg '[title="terminal \[1\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
+                case "$2" in
+                    "top")
+                        make_terminal_full_screen "dropdown_terminal [1]" "top"
+                        ;;
+
+                    "right")
+                        make_terminal_full_screen "dropdown_terminal [1]" "right"
+                        ;;
+                esac
             fi
-        else
-            i3-msg '[title="terminal \[2\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
+        else # dropdown_terminal [1] is being moved to the scratchpad
+            # maximixe dropdown_terminal [2], it does'nt matter if it's present or not
+            case "$2" in
+                "top")
+                    make_terminal_full_screen "dropdown_terminal [2]" "top"
+                    ;;
+
+                "right")
+                    make_terminal_full_screen "dropdown_terminal [2]" "right"
+                    ;;
+            esac
         fi
+
         ;;
 
-    "terminal [2]")
-        i3-msg '[title="terminal \[2\]"] scratchpad show'
+    "dropdown_terminal [2]")
+        i3-msg '[title="dropdown_terminal \[2\]"] scratchpad show'
         i3-msg '[title="cmus"] move scratchpad'
 
         sleep 0.1
         current_window=$(xdotool getactivewindow getwindowname)
 
-        if [ "$current_window" = "terminal [2]" ]
+        if [ "$current_window" = "dropdown_terminal [2]" ]
         then
-            if [ "$previous_window" = "terminal [1]" ]
+            if [ "$previous_window" = "dropdown_terminal [1]" ]
             then
-                i3-msg '[title="terminal \[2\]"] move absolute position 967 px 47 px, resize set 939 px 900 px'
-                i3-msg '[title="terminal \[1\]"] move absolute position 14 px 47 px, resize set 939 px 900 px'
+                case "$2" in
+
+                    "top")
+                        # split the screen between dropdown_terminal [1] and dropdown_terminal [2]
+                        split_screen_between_terminals "top"
+                        ;;
+
+                    "right")
+                        # split the screen between dropdown_terminal [1] and dropdown_terminal [2]
+                        split_screen_between_terminals "right"
+                        ;;
+
+                esac
             else
-                i3-msg '[title="terminal \[2\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
+                case "$2" in
+                    "top")
+                        make_terminal_full_screen "dropdown_terminal [2]" "top"
+                        ;;
+
+                    "right")
+                        make_terminal_full_screen "dropdown_terminal [2]" "right"
+                        ;;
+                esac
             fi
         else
-            i3-msg '[title="terminal \[1\]"] move absolute position 14 px 47 px, resize set 1892 px 900 px'
+            case "$2" in
+                "top")
+                    make_terminal_full_screen "dropdown_terminal [1]" "top"
+                    ;;
+
+                "right")
+                    make_terminal_full_screen "dropdown_terminal [1]" "right"
+                    ;;
+            esac
         fi
         ;;
 
