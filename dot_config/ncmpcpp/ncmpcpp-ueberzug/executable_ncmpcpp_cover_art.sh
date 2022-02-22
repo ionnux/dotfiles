e56597330b5e06dc61de88b9e8241f361733b6e1
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-02-22 07:53:19 +0100

    Update dot_config/greg/greg.conf Update dot_config/mpd/mpd.conf Update dot_config/ncmpcpp/config Update dot_config/ncmpcpp/ncmpcpp-ueberzug/executable_ncmpcpp_cover_art.sh

--- dot_config/ncmpcpp/ncmpcpp-ueberzug/executable_ncmpcpp_cover_art.sh
+++ dot_config/ncmpcpp/ncmpcpp-ueberzug/executable_ncmpcpp_cover_art.sh
@@ -2,7 +2,7 @@
 # Cover art script for ncmpcpp-ueberzug
 
 # SETTINGS
-music_library="$HOME/Music/Albums/"
+music_library="$HOME/Music/"
 fallback_image="$HOME/.config/ncmpcpp/ncmpcpp-ueberzug/img/fallback.png"
 padding_top=3
 padding_bottom=1
