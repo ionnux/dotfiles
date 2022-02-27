a06b0058b1a5a74acc8dfaa00659c342a68450d3
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-02-27 20:07:42 +0100

    changes made

--- dot_config/surfingkeys/surfingkeys.js
+++ dot_config/surfingkeys/surfingkeys.js
@@ -24,29 +24,447 @@ map("K", "R");
 map("J", "E");
 map("H", "S");
 map("L", "D");
-vmapkey("gs", "sg")
+// vmapkey('<Ctrl-[>', "<Esc>")
 imap('jk', "<Esc>");
-imap('<Ctrl-i>', "<Bs>");
+imap('<Ctrl-w>', "<Alt-w>");
+imap('<Ctrl-d>', "<Alt-d>");
+map('<Ctrl-Space>', 'og');
 
 aceVimMap('jk', '<Esc>', 'insert');
 aceVimMap('<Ctrl-h>', '<Bs>', 'insert');
 // aceVimMap('<Alt-w>', '<Ctrl-Bs>', 'insert');
 
 // omnibar mappings
-// cmap('<Ctrl-n>', '<Tab>');
-// cmap('<Ctrl-p>', '<Shift-Tab>');
+// settings.omnibarPosition = 'bottom'
 
-// settings.smoothScroll = false;
-// settings.scrollStepSize = 140;
+settings.smoothScroll = true;
+settings.scrollStepSize = 140;
 
-// mapkey('<Ctrl-z>', 'fill in login form', function() {
-//  const origin = window.location.origin
-//     if(origin === "http://192.168.0.1"){           
-//    window.document.getElementById("txtPwd").value = "Qetuo0112" 
-//    window.document.getElementById("btnLogin").click() 
-//    return 
-//  }
-// });
+// settings.tabsThreshold = 0;
+// settings.tabsMRUOrder = false;
+
+mapkey('<Ctrl-z>', 'fill in login form', function() {
+ const origin = window.location.origin
+    if(origin === "http://192.168.0.1"){          
+   window.document.getElementById("txtPwd").value = "Qetuo0112"
+   window.document.getElementById("btnLogin").click()
+   return
+ }
+});
 
 settings.hintAlign = "left";
 settings.useNeovim = false
+
+
+// toggle fullscreen, mainly because of YouTube
+mapkey('<Alt-u>', 'Fullscreen', function() {
+    if (window.fullScreen) {
+        document.exitFullscreen();
+    }
+    else {
+        document.documentElement.requestFullscreen();
+    }
+});
+
+
+// themes
+
+// ---- Hints ----
+// Hints have to be defined separately
+// Uncomment to enable
+
+// Tomorrow-Night
+// Hints.style('border: solid 2px #373B41; color:#52C196; background: initial; background-color: #1D1F21;');
+// Hints.style("border: solid 2px #373B41 !important; padding: 1px !important; color: #C5C8C6 !important; background: #1D1F21 !important;", "text");
+// Visual.style('marks', 'background-color: #52C19699;');
+// Visual.style('cursor', 'background-color: #81A2BE;');
+
+// TOKYONIGHT
+Hints.style('border: solid 2px #565f89; color:#bb9af7; background: initial; background-color: #24283b;');
+Hints.style("border: solid 2px #565f89 !important; padding: 1px !important; color: #7aa2f7 !important; background: #24283b !important;", "text");
+Visual.style('marks', 'background-color: #52C19699;');
+Visual.style('cursor', 'background-color: #7aa2f7;');
+
+// Nord
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #4C566A; color:#A3BE8C; background: initial; background-color: #3B4252;');
+Hints.style("border: solid 2px #4C566A !important; padding: 1px !important; color: #E5E9F0 !important; background: #3B4252 !important;", "text");
+Visual.style('marks', 'background-color: #A3BE8C99;');
+Visual.style('cursor', 'background-color: #88C0D0;');
+-- DELETE LINE TO ENABLE THEME */
+
+// Doom One
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #282C34; color:#98be65; background: initial; background-color: #2E3440;');
+Hints.style("border: solid 2px #282C34 !important; padding: 1px !important; color: #51AFEF !important; background: #2E3440 !important;", "text");
+Visual.style('marks', 'background-color: #98be6599;');
+Visual.style('cursor', 'background-color: #51AFEF;');
+-- DELETE LINE TO ENABLE THEME */
+
+// Monokai
+/* -- DELETE LINE TO ENABLE THEME
+Hints.style('border: solid 2px #2D2E2E; color:#F92660; background: initial; background-color: #272822;');
+Hints.style("border: solid 2px #2D2E2E !important; padding: 1px !important; color: #A6E22E !important; background: #272822 !important;", "text");
+Visual.style('marks', 'background-color: #A6E22E99;');
+Visual.style('cursor', 'background-color: #F92660;');
+-- DELETE LINE TO ENABLE THEME */
+
+settings.theme = `
+/* Edit these variables for easy theme making */
+:root {
+  /* Font */
+  --font: Iosevka Nerd Font;
+  --font-size: 17;
+  --font-weight: medium;
+
+  /* -------------- */
+  /* --- THEMES --- */
+  /* -------------- */
+
+  /* -------------------- */
+  /* -- Tomorrow Night -- */
+  /* -------------------- */
+  --fg: #C5C8C6;
+  --bg: #282A2E;
+  --bg-dark: #1D1F21;
+  --border: #373b41;
+  --main-fg: #81A2BE;
+  --accent-fg: #52C196;
+  --info-fg: #AC7BBA;
+  --select: #585858;
+
+  /* Unused Alternate Colors */
+  /* --cyan: #4CB3BC; */
+  /* --orange: #DE935F; */
+  /* --red: #CC6666; */
+  /* --yellow: #CBCA77; */
+
+  /* -------------------- */
+  /* --      NORD      -- */
+  /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
+  --fg: #E5E9F0;
+  --bg: #3B4252;
+  --bg-dark: #2E3440;
+  --border: #4C566A;
+  --main-fg: #88C0D0;
+  --accent-fg: #A3BE8C;
+  --info-fg: #5E81AC;
+  --select: #4C566A;
+  -- DELETE LINE TO ENABLE THEME */
+
+  /* Unused Alternate Colors */
+  /* --orange: #D08770; */
+  /* --red: #BF616A; */
+  /* --yellow: #EBCB8B; */
+
+  /* -------------------- */
+  /* --    DOOM ONE    -- */
+  /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
+  --fg: #51AFEF;
+  --bg: #2E3440;
+  --bg-dark: #21242B;
+  --border: #2257A0;
+  --main-fg: #51AFEF;
+  --accent-fg: #98be65;
+  --info-fg: #C678DD;
+  --select: #4C566A;
+  -- DELETE LINE TO ENABLE THEME */
+
+  /* Unused Alternate Colors */
+  /* --border-alt: #282C34; */
+  /* --cyan: #46D9FF; */
+  /* --orange: #DA8548; */
+  /* --red: #FF6C6B; */
+  /* --yellow: #ECBE7B; */
+
+  /* ---------------------- */
+  /* --    TOKYONIGHT    -- */
+  /* ---------------------- */
+  --fg: var(--fg);
+  --bg: var(--bg);
+  --bg-dark: var(--bg_dark);
+  --border: var(--fg_gutter);
+  --main-fg: var(--fg);
+  --accent-fg: var(--blue);
+  --info-fg: var(--magenta);
+  --select: var(--comment);
+
+  --bg_dark: #1f2335;
+  --bg: #24283b;
+  --bg_highlight: #292e42;
+  --terminal_black: #414868;
+  --fg: #c0caf5;
+  --fg_dark: #a9b1d6;
+  --fg_gutter: #3b4261;
+  --dark3: #545c7e;
+  --comment: #565f89;
+  --dark5: #737aa2;
+  --blue0: #3d59a1;
+  --blue: #7aa2f7;
+  --cyan: #7dcfff;
+  --blue1: #2ac3de;
+  --blue2: #0db9d7;
+  --blue5: #89ddff;
+  --blue6: #B4F9F8;
+  --blue7: #394b70;
+  --magenta: #bb9af7;
+  --magenta2: #ff007c;
+  --purple: #9d7cd8;
+  --orange: #ff9e64;
+  --yellow: #e0af68;
+  --green: #9ece6a;
+  --green1: #73daca;
+  --green2: #41a6b5;
+  --teal: #1abc9c;
+  --red: #f7768e;
+  --red1: #db4b4b;
+
+
+  /* -------------------- */
+  /* --    MONOKAI    -- */
+  /* -------------------- */
+  /* -- DELETE LINE TO ENABLE THEME
+  --fg: #F8F8F2;
+  --bg: #272822;
+  --bg-dark: #1D1E19;
+  --border: #2D2E2E;
+  --main-fg: #F92660;
+  --accent-fg: #E6DB74;
+  --info-fg: #A6E22E;
+  --select: #556172;
+  -- DELETE LINE TO ENABLE THEME */
+
+  /* Unused Alternate Colors */
+  /* --red: #E74C3C; */
+  /* --orange: #FD971F; */
+  /* --blue: #268BD2; */
+  /* --violet: #9C91E4; */
+  /* --cyan: #66D9EF; */
+}
+
+/* ---------- Generic ---------- */
+.sk_theme {
+background: var(--bg);
+color: var(--fg);
+  background-color: var(--bg);
+  border-color: var(--border);
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+
+input {
+  font-family: var(--font);
+  font-weight: var(--font-weight);
+}
+
+.sk_theme tbody {
+  color: var(--fg);
+}
+
+.sk_theme input {
+  color: var(--fg);
+}
+
+/* Hints */
+#sk_hints .begin {
+  color: var(--accent-fg) !important;
+}
+
+#sk_tabs .sk_tab {
+  background: var(--bg-dark);
+  border: 1px solid var(--border);
+}
+
+#sk_tabs .sk_tab_title {
+  color: var(--fg);
+}
+
+#sk_tabs .sk_tab_url {
+  color: var(--main-fg);
+}
+
+#sk_tabs .sk_tab_hint {
+  background: var(--bg);
+  border: 1px solid var(--border);
+  color: var(--accent-fg);
+}
+
+.sk_theme #sk_frame {
+  background: var(--bg);
+  opacity: 0.2;
+  color: var(--accent-fg);
+}
+
+/* ---------- Omnibar ---------- */
+/* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
+.sk_theme#sk_omnibar {
+  width: 80%;
+  left: 10%;
+  top: 0%;
+  // bottom: 0%;
+}
+
+.sk_theme .title {
+  color: var(--accent-fg);
+}
+
+.sk_theme .url {
+  color: var(--main-fg);
+}
+
+.sk_theme .annotation {
+  color: var(--accent-fg);
+}
+
+.sk_theme .omnibar_highlight {
+  color: var(--accent-fg);
+}
+
+.sk_theme .omnibar_timestamp {
+  color: var(--info-fg);
+}
+
+.sk_theme .omnibar_visitcount {
+  color: var(--accent-fg);
+}
+
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: var(--bg-dark);
+}
+
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: var(--border);
+}
+
+.sk_theme #sk_omnibarSearchArea {
+  border-top-color: var(--border);
+  border-bottom-color: var(--border);
+}
+
+.sk_theme #sk_omnibarSearchArea input,
+.sk_theme #sk_omnibarSearchArea span {
+  font-size: var(--font-size);
+}
+
+.sk_theme .separator {
+  color: var(--accent-fg);
+}
+
+/* ---------- Popup Notification Banner ---------- */
+#sk_banner {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  background: var(--bg);
+  border-color: var(--border);
+  color: var(--fg);
+  opacity: 0.9;
+}
+
+/* ---------- Popup Keys ---------- */
+#sk_keystroke {
+  background-color: var(--bg);
+}
+
+.sk_theme kbd .candidates {
+  color: var(--info-fg);
+}
+
+.sk_theme span.annotation {
+  color: var(--accent-fg);
+}
+
+/* ---------- Popup Translation Bubble ---------- */
+#sk_bubble {
+  background-color: var(--bg) !important;
+  color: var(--fg) !important;
+  border-color: var(--border) !important;
+}
+
+#sk_bubble * {
+  color: var(--fg) !important;
+}
+
+#sk_bubble div.sk_arrow div:nth-of-type(1) {
+  border-top-color: var(--border) !important;
+  border-bottom-color: var(--border) !important;
+}
+
+#sk_bubble div.sk_arrow div:nth-of-type(2) {
+  border-top-color: var(--bg) !important;
+  border-bottom-color: var(--bg) !important;
+}
+
+/* ---------- Search ---------- */
+#sk_status,
+#sk_find {
+  font-size: 12;
+  border-color: var(--fg_gutter);
+  bottom: 5;
+  right: 0;
+}
+
+.sk_theme kbd {
+  background: var(--bg-dark);
+  border-color: var(--border);
+  box-shadow: none;
+  color: var(--fg);
+}
+
+.sk_theme .feature_name span {
+  color: var(--main-fg);
+}
+
+/* ---------- ACE Editor ---------- */
+#sk_editor {
+  background: var(--bg-dark) !important;
+  height: 20% !important;
+  top: 0%;
+  width: 80%;
+}
+
+.ace_dialog-bottom {
+  border-top: 1px solid var(--bg) !important;
+}
+
+.ace-chrome .ace_print-margin,
+.ace_gutter,
+.ace_gutter-cell,
+.ace_dialog {
+  background: var(--bg) !important;
+}
+
+.ace-chrome {
+  color: var(--fg) !important;
+}
+
+.ace_gutter,
+.ace_dialog {
+  color: var(--fg) !important;
+}
+
+.ace_cursor {
+  color: var(--fg) !important;
+}
+
+.normal-mode .ace_cursor {
+  background-color: var(--fg) !important;
+  border: var(--fg) !important;
+  opacity: 0.7 !important;
+}
+
+.ace_marker-layer .ace_selection {
+  background: var(--select) !important;
+}
+
+.ace_editor,
+.ace_dialog span,
+.ace_dialog input {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+`;
