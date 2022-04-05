9116962a93f2b150eda3e2dff25126630a6d3119
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-04-05 15:41:21 +0100

    Add dot_config/rofi/scripts/executable_dual_monitor.sh Add dot_config/rofi/scripts/executable_kdeconnect.sh Add dot_config/rofi/scripts/executable_kdeconnect_sendfile.sh Add dot_config/rofi/scripts/executable_launcher.sh Add dot_config/rofi/scripts/executable_wifi.sh

---
+++ dot_config/rofi/scripts/executable_dual_monitor.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+secondary_resolution="1200x1920"
+secondary_resolution_x="$( awk -F 'x' '{print $1}' <<< "$secondary_resolution")"
+secondary_resolution_y="$( awk -F 'x' '{print $2}' <<< "$secondary_resolution")"
+refresh_rate="60"
+secondary_name="VIRTUAL1"
+primary_name="$(xrandr --listactivemonitors | awk 'FNR == 2 {print $4}')"
+primary_resolution_x=$(xrandr | grep $primary_name | grep -E -o '[[:digit:]]{4}x[[:digit:]]{4}' | awk -F 'x' '{print $1}')
+primary_resolution_y=$(xrandr | grep $primary_name | grep -E -o '[[:digit:]]{4}x[[:digit:]]{4}' | awk -F 'x' '{print $2}')
+secondary_position="--right-of $primary_name"
+
+
+add_secondary_monitor() {
+    xrandr_mode=$(gtf $secondary_resolution_x $secondary_resolution_y $refresh_rate | awk -F'Modeline ' 'FNR == 3 {print $2}')
+    eval "xrandr --newmode $xrandr_mode"
+    eval "xrandr --addmode "$secondary_name" "${secondary_resolution}_${refresh_rate}.00""
+    eval "xrandr --output $secondary_name --mode "${secondary_resolution}_${refresh_rate}.00" $secondary_position"
+
+    if ! pgrep -f -a "polybar -q $secondary_name"; then
+        polybar -q $secondary_name -c "~/.config/polybar/blocks/config.ini" &
+    fi
+
+    if ! pgrep -f -a "polybar -q $primary_name"; then
+        polybar -q $primary_name -c "~/.config/polybar/blocks/config.ini" &
+    fi
+
+    ~/.fehbg
+}
+
+remove_secondary_monitor() {
+    eval "xrandr --output $secondary_name --off"
+    eval "xrandr --delmode "$secondary_name" "${secondary_resolution}_${refresh_rate}.00""
+    eval "xrandr --rmmode "${secondary_resolution}_${refresh_rate}.00""
+
+    if pgrep -f -a "polybar -q $secondary_name"; then
+        pkill -f "polybar -q $secondary_name"
+    fi
+
+    if ! pgrep -f -a "polybar -q $primary_name"; then
+        polybar -q $primary_name -c "~/.config/polybar/blocks/config.ini" &
+    fi
+
+    ~/.fehbg
+    # ~/.config/i3/i3Scripts/change_display_properties.sh -a
+}
+
+start_vnc_server() {
+    eval 'x11vnc -nocursorshape -nocursorpos -solid "#4c566a" -forever -rfbport 5901 -fs 0 -bg -noprimary -repeat -xdamage -display :0 -wireframe -clip ${secondary_resolution}+${primary_resolution_x}+0'
+    pkill picom
+    dunstify -h string:x-dunst-stack-tag:Vnc "Vnc" -h string:x-dunst-stack-tag:Vnc "server started"
+}
+
+stop_vnc_server() {
+    pkill x11vnc
+    dunstify -h string:x-dunst-stack-tag:Vnc "Vnc" -h string:x-dunst-stack-tag:Vnc "server stopped"
+    if ! pgrep -a picom; then
+        picom --experimental-backends &
+    fi
+}
+
+start () {
+    add_secondary_monitor
+    sleep 0.2
+    if ! pgrep -a x11vnc; then
+        start_vnc_server
+    fi
+}
+
+stop () {
+    stop_vnc_server
+    sleep 0.2
+    remove_secondary_monitor
+}
+
+show_menu () {
+    if pgrep -a x11vnc; then
+        is_vnc_server_active="yes"
+        vnc_server_option="Stop vnc server"
+    else
+        is_vnc_server_active="no"
+        vnc_server_option="Start vnc server"
+    fi
+
+    if [[ "$(xrandr --listactivemonitors | grep Monitors | awk -F': ' '{print $2}')" -gt "1" ]]; then
+        is_dual_monitor_active="yes"
+        monitor_option="Remove secondary monitor"
+    else
+        is_dual_monitor_active="no"
+        monitor_option="Add secondary monitor"
+    fi
+
+    if [[ "$is_dual_monitor_active" == "yes" ]] && [[ "$is_vnc_server_active" == "yes" ]]; then
+        start_option="Stop dual monitor mode|"
+    elif [[ "$is_dual_monitor_active" == "no" ]] && [[ "$is_vnc_server_active" == "no" ]]; then
+        start_option="Launch dual monitor mode|"
+    fi
+
+    focused_output=$(bspc query --monitors -m focused --names)
+    if [ "$focused_output" = "eDP1" ]; then
+        font="Iosevka 13"
+    else
+        font="Iosevka 16"
+    fi
+
+    menu="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -theme-str "configuration {font: \"$font\";}" -p "Dual Monitor" -i <<< "${start_option}Adb|$vnc_server_option|$monitor_option")"
+    case "$menu" in
+        'Launch dual monitor mode') start ;;
+        'Stop dual monitor mode') stop ;;
+        'Adb') adb reverse tcp:5901 tcp:5901 ;;
+        'Stop vnc server') stop_vnc_server ;;
+        'Start vnc server') start_vnc_server ;;
+        'Add secondary monitor') add_secondary_monitor ;;
+        'Remove secondary monitor') remove_secondary_monitor ;;
+    esac
+}
+
+while getopts ':r:h:s:p:' opt; do
+    case "$opt" in
+        r)
+            if [[ $OPTARG =~ ^[0-9]{4}x[0-9]{4}$ ]]; then
+                secondary_resolution="$OPTARG"
+            fi
+            ;;
+        h)
+            if [[ $OPTARG =~ ^[0-9]{2,3}$ ]]; then
+                refresh_rate="$OPTARG"
+            fi
+            ;;
+        s) secondary_name="$OPTARG" ;;
+        p) secondary_position="--${OPTARG}-of $primary_name" ;;
+    esac
+done
+
+show_menu
---
+++ dot_config/rofi/scripts/executable_kdeconnect.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+
+pairing_device_name=""
+pairing_device_id=""
+selected_device_name=""
+selected_device_id=""
+has_pairing="false"
+device_names=""
+
+focused_output=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).output')
+if [ "$focused_output" = "eDP1" ]; then
+    font="Iosevka 13"
+else
+    font="Iosevka 16"
+fi
+
+start () {
+    counter=0
+    for device in $(qdbus --literal org.kde.kdeconnect /modules/kdeconnect org.kde.kdeconnect.daemon.devices); do
+        deviceid=$(echo "$device" | awk -F'["|"]' '{print $2}')
+        devicename=$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$deviceid" org.kde.kdeconnect.device.name)
+
+        check_pairing "$devicename" "$deviceid"
+        add_device_name "$devicename" "$deviceid"
+    done
+    if [ "$has_pairing" = "false" ]; then
+        if [ "$device_names" = "" ]; then #if there are no devices to show
+            show_empty
+        else
+            show_devices
+        fi
+    else #if one of the device is asking to pair.
+        show_pair_action_menu "$pairing_device_name" "$pairing_device_id"
+    fi
+
+}
+
+add_device_name() {
+    device_name="$1"
+    is_paired=$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.isTrusted)
+    is_reachable="$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.isReachable)"
+
+    if [ $is_paired = false ]; then
+        devicename="$1 [pair]"
+    fi
+
+    if [ $is_reachable = true ]; then
+        if [ $counter -gt 0 ]; then
+            device_names="$device_names|$devicename"
+        else
+            device_names="$devicename"
+        fi
+
+        counter=$((counter + 1))
+    fi
+
+}
+
+check_pairing() {
+    has_pairing="$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.hasPairingRequests)"
+    if [ "$has_pairing" = "true" ]; then
+        pairing_device_name="$1"
+        pairing_device_id="$2"
+    fi
+}
+
+select_device (){
+    for device in $(qdbus --literal org.kde.kdeconnect /modules/kdeconnect org.kde.kdeconnect.daemon.devices); do
+        deviceid=$(echo "$device" | awk -F'["|"]' '{print $2}')
+        devicename=$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$deviceid" org.kde.kdeconnect.device.name)
+
+        if [ "$1" = "$devicename" ] || [ "$1" = "$devicename [pair]" ]; then
+            selected_device_id="$deviceid"
+            selected_device_name="$devicename"
+        fi
+    done
+}
+
+show_device_list_menu () {
+    menu="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -i -theme-str "configuration {font: \"$font\";}" -p "$selected_device_name" <<< "Ping|Find Device|Send File|Browse Files|Unpair")"
+    case "$menu" in
+        *'Ping') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$selected_device_id/ping" org.kde.kdeconnect.device.ping.sendPing ;;
+        *'Find Device') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$selected_device_id/findmyphone" org.kde.kdeconnect.device.findmyphone.ring ;;
+        *'Send File') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$selected_device_id/share" org.kde.kdeconnect.device.share.shareUrl "file://$(zenity --file-selection)" ;;
+        *'Browse Files')
+            if [ "$(qdbus --literal org.kde.kdeconnect /modules/kdeconnect/devices/99c2205bc9b97222/sftp org.kde.kdeconnect.device.sftp.isMounted)" = "false" ]
+            then
+                qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$selected_device_id/sftp" org.kde.kdeconnect.device.sftp.mount
+            fi
+            sleep 0.5
+            i3-msg '[title="dropdown_vifm"] kill'
+            ~/.local/kitty.app/bin/kitty --title dropdown_vifm ~/.config/vifm/scripts/vifmrun $(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$selected_device_id/sftp" org.kde.kdeconnect.device.sftp.mountPoint) ;;
+        *'Unpair' ) qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$selected_device_id" org.kde.kdeconnect.device.unpair ;;
+    esac
+}
+
+
+show_pair_action_menu () {
+    menu="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -theme-str "configuration {font: \"$font\";}" -i -p "$1 has sent a pairing request" <<< "Accept|Reject")"
+    case "$menu" in
+        *'Accept') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.acceptPairing ;;
+        *) qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.rejectPairing
+    esac
+
+}
+request_pair() {
+    qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$1" org.kde.kdeconnect.device.requestPair
+}
+
+show_devices () {
+    if ! [[ "$device_names" == *\|* ]]; then
+        if [[ "$device_names" == *pair* ]]; then
+            auto_select=""
+        else
+            auto_select="-auto-select"
+        fi
+    fi
+
+    menu="$(rofi -sep "|" "$auto_select" -dmenu -config ~/.config/rofi/dmenu.rasi -theme-str "configuration {font: \"$font\";}" -i -p "kdeconnect" <<< "$device_names")"
+    case "$menu" in
+        *)
+            select_device "$menu"
+            if [ "$selected_device_id" != "" ]; then
+                if [[ $menu = *pair* ]]; then
+                    request_pair "$selected_device_id"
+                else
+                    show_device_list_menu
+                fi
+            fi
+            ;;
+    esac
+}
+
+show_empty () {
+    menu="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -theme-str "configuration {font: \"$font\";}" -i -p "kdeconnect" <<< "No device connected.")"
+}
+
+start
