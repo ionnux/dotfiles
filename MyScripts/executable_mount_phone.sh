eed7cb1bd29904f6163c43e8ca352f72111b0d10
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-04-11 21:32:56 +0100

    Update MyScripts/executable_mount_phone.sh

--- MyScripts/executable_mount_phone.sh
+++ MyScripts/executable_mount_phone.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+count=1
 fusermount -uzq ~/mnt
-while [[ -z $(ls ~/mnt) ]]; do
+while [[ -z $(ls ~/mnt) && count -le 2 ]]; do
     go-mtpfs ~/mnt
+    count=$(( count + 1 ))
+    echo count
     sleep 4
 done
+echo done
