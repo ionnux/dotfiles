bb99c18008ed39fa631c7967da32954a8aba78d4
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-04-13 14:59:05 +0100

    Update dot_config/private_mpv/mpv.conf Update dot_config/private_vifm/vifmrc Update dot_config/rofi/scripts/executable_launcher.sh

--- dot_config/rofi/scripts/executable_launcher.sh
+++ dot_config/rofi/scripts/executable_launcher.sh
@@ -2,10 +2,11 @@
 
 focused_output=$(bspc query --monitors -m focused --names)
 if [ "$focused_output" = "eDP1" ]; then
-    font="Iosevka 13"
+    font="Iosevka 12"
 else
     font="Iosevka 16"
 fi
 
 rofi -show drun -display-drun launcher -config ~/.config/rofi/dmenu.rasi -theme-str "configuration {font: \"$font\";}"
+# rofi -show drun -display-drun launcher -config ~/.config/rofi/dmenu.rasi
 
