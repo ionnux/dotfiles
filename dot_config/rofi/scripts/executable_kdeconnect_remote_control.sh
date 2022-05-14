a855928943585c6ec701e0ce0da5fc60eaa24d2b
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-05-14 17:43:15 +0100

    Update dot_config/rofi/scripts/executable_kdeconnect_remote_control.sh

--- dot_config/rofi/scripts/executable_kdeconnect_remote_control.sh
+++ dot_config/rofi/scripts/executable_kdeconnect_remote_control.sh
@@ -1,34 +1,39 @@
 #!/usr/bin/bash
+persistent_menu='yes'
 source ~/MyScripts/rofi_font_size.sh
 source ~/.config/dunst/scripts/remote_command_variables.sh
 rofi_config='-sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -i'
 
 show_device_menu() {
+    # sed -i s/data_balance=.*/data_balance=""/ ~/.config/dunst/scripts/remote_command_variables.sh
     local selected_command="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -theme-str "configuration {font: \"$font\";}" -i\
-      -p "Kdeconnect Remote Command" <<< "Mobile Data [$mobile_data]|Bluetooth [$bluetooth]|Flash [$flash]|Open Link")"
+      -p "$1" <<< "Mobile Data $mobile_data|Bluetooth $bluetooth|Torch $torch|Open Link|Data Balance $data_balance")"
 
     if [[ $selected_command == Bluetooth* ]]; then
         kdeconnect-cli --ping-msg "Remote Command:$selected_command" --name "$1"
     elif [[ $selected_command == "Mobile Data"* ]]; then
         kdeconnect-cli --ping-msg "Remote Command:$selected_command" --name "$1"
-    elif [[ $selected_command == Flash* ]]; then
+    elif [[ $selected_command == Torch* ]]; then
+        kdeconnect-cli --ping-msg "Remote Command:$selected_command" --name "$1"
+    elif [[ $selected_command == "Data Balance"* ]]; then
         kdeconnect-cli --ping-msg "Remote Command:$selected_command" --name "$1"
     elif [[ $selected_command == "Open Link" ]]; then
         local clipboard="$(xclip -o)"
         local regex='^(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$'
+
         if [[ $clipboard =~ $regex ]]; then
             local link="$clipboard"
         else
-            local input="$(eval "rofi $rofi_config -theme-str 'configuration { font: \"$font\"; }' -theme-str 'entry { placeholder: \"   Link...\"; }' -p 'Enter Link'")"
-            if [[ $input =~ $regex ]]; then
-                local link="$input"
-            else
-                if [[ ! -z $input ]]; then local link="https://www.google.com/search?q=$input"; fi
+            local link="$(eval "rofi $rofi_config -theme-str 'configuration { font: \"$font\"; }' -theme-str 'entry { placeholder: \"   Link...\"; }' -p 'Enter Link'")"
+            if [[ ! $link =~ $regex ]]; then
+                if [[ ! -z $link ]]; then local link="https://www.google.com/search?q=$link"; fi
             fi
         fi
 
         if [[ ! -z $link ]]; then kdeconnect-cli --ping-msg "Remote Command:${selected_command}:${link}" --name "$1"; fi
     fi
+
+    if [[ ! -z $selected_command && $persistent_menu == yes ]]; then show_device_menu "$1"; fi
 }
 
 show_available_devices() {
@@ -51,7 +56,7 @@ show_available_devices() {
             fi
         done
 
-        local selected_device="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -auto-select -theme-str "configuration {font: \"$font\";}" -i -p "wifi" <<< "${rofi_device_name}")"
+        local selected_device="$(rofi -sep "|" -dmenu -config ~/.config/rofi/dmenu.rasi -auto-select -theme-str "configuration {font: \"$font\";}" -i -p "Remote Command" <<< "${rofi_device_name}")"
 
         if [[ ! -z $selected_device ]]; then
             show_device_menu "$selected_device"
