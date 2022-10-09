6ca874a2ac5f6ce2db90d996fa04a42daf16f5a0
Sadiq Abdulazeez <sadiq_abdulazeez@outlook.com>
2022-10-09 10:25:02 +0100

    Add dot_config/nvim/lua/config/borders.lua Update dot_config/nvim/lua/config/formatter.lua Update dot_config/nvim/lua/config/lsp.lua Add dot_config/nvim/lua/config/lsp/.keep Add dot_config/nvim/lua/config/luasnip/init.lua Update dot_config/nvim/lua/config/luasnip/vscode_placeholder.lua

--- dot_config/nvim/lua/config/formatter.lua
+++ dot_config/nvim/lua/config/formatter.lua
@@ -1,50 +1,50 @@
-require("formatter").setup({
-	logging = false,
-	filetype = {
-		dart = {
-			-- dart format
-			function()
-				return { exe = "dart format", args = { "--fix" }, stdin = true }
-			end,
-		},
+require('formatter').setup {
+  logging = false,
+  filetype = {
+    dart = {
+      -- dart format
+      function()
+        return { exe = 'dart format', args = { '--fix' }, stdin = true }
+      end,
+    },
 
-		sh = {
-			function()
-				return { exe = "beautysh", stdin = false }
-			end,
-		},
+    sh = {
+      function()
+        return { exe = 'beautysh', stdin = false }
+      end,
+    },
 
-		zsh = {
-			function()
-				return { exe = "beautysh", stdin = false }
-			end,
-		},
+    zsh = {
+      function()
+        return { exe = 'beautysh', stdin = false }
+      end,
+    },
 
-		lua = {
-			function()
-				return {
-					exe = "stylua",
-					args = { "--config-path ~/.config/stylua/stylua.toml" },
-					stdin = false,
-				}
-			end,
-		},
+    lua = {
+      function()
+        return {
+          exe = 'stylua',
+          args = { '--config-path ~/.config/stylua/stylua.toml' },
+          stdin = false,
+        }
+      end,
+    },
 
-		-- elixir = {
-		-- 	function()
-		-- 		return { exe = "mix format", stdin = false }
-		-- 	end,
-		-- },
-	},
-})
+    -- elixir = {
+    -- 	function()
+    -- 		return { exe = "mix format", stdin = false }
+    -- 	end,
+    -- },
+  },
+}
 
 -- format on save
 vim.api.nvim_exec(
-	[[
+  [[
 augroup FormatAutogroup
   autocmd!
   autocmd BufWritePost *.dart,*.lua,*.sh,*zshrc FormatWrite
 augroup END
 ]],
-	true
+  true
 )
---
+++ dot_config/nvim/lua/config/luasnip/init.lua
@@ -0,0 +1,33 @@
+local ls = require('luasnip')
+local types = require('luasnip.util.types')
+
+ls.config.setup({
+  history = false,
+  -- updateevents = 'InsertLeave',
+  updateevents = 'InsertLeave',
+  enable_autosnippets = true,
+  region_check_events = 'CursorHold',
+  delete_check_events = 'TextChanged',
+  -- store_selection_keys = "<Tab>",
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = { { ' Choice', 'Error' } },
+      },
+    },
+    [types.insertNode] = {
+      active = {
+        virt_text = { { '● ', 'Comment' } },
+      },
+    },
+  },
+})
+
+-- activate ChoiceNode popup
+-- require("config.luasnip.choice_node_popup")
+-- activate vscode placeholder
+-- require("config.luasnip.vscode_placeholder")
+
+vim.api.nvim_set_keymap('i', '<c-n>', '<Plug>luasnip-next-choice', {})
+vim.api.nvim_set_keymap('i', '<c-p>', '<Plug>luasnip-prev-choice', {})
+require('luasnip.loaders.from_vscode').load({ paths = './snippets' })
